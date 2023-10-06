3259d84c3cd77794ef7b5834a1008037e0d3eccd
Amaury Guérin <amaugofast@MBP-de-Amaury.lan>
2023-10-06 17:56:49 +0200

    FRONT // création layout de base et recette card BACK // connexion bdd locale

--- resources/js/App.vue
+++ resources/js/App.vue
@@ -1,5 +1,38 @@
 <template>
-    <v-btn append-icon="$vuetify" variant="outlined">
-    OK ça marche
-    </v-btn>
+    <v-app>
+        <v-app-bar>
+            <v-app-bar-nav-icon icon="mdi-home" href="/"></v-app-bar-nav-icon>
+            <v-app-bar-title>Recettes de grand mère</v-app-bar-title>
+        </v-app-bar>
+        <v-main>
+            <v-container>
+                <v-row>
+                    <v-col v-for="n in 5" :key="n" cols="3">
+                        <RecipeCard />
+                    </v-col>
+                </v-row>
+            </v-container>
+            <v-btn icon="$plus" size="x-large"></v-btn>
+        </v-main>
+    </v-app>
 </template>
+
+<script>
+import RecipeCard from '@/components/RecipeCard.vue';
+
+export default {
+    name: 'App',
+    components: {
+        RecipeCard,
+    }
+}
+</script>
+
+<style lang="scss" scoped>
+.v-btn {
+    position: fixed;
+    bottom: 10rem;
+    right: 10rem;
+    color: lightcoral;
+}
+</style>
---
+++ resources/js/components/RecipeCard.vue
@@ -0,0 +1,45 @@
+<template>
+    <v-card :loading="loading" class="mx-auto my-12" max-width="374">
+        <template v-slot:loader="{ isActive }">
+            <v-progress-linear :active="isActive" color="deep-purple" height="4" indeterminate></v-progress-linear>
+        </template>
+        <v-img cover height="250" src="https://cdn.vuetifyjs.com/images/cards/cooking.png"></v-img>
+        <v-card-item>
+            <v-card-title>Raclette savoyarde</v-card-title>
+            <v-card-subtitle>
+                Temps de préparation : ...
+            </v-card-subtitle>
+            <v-card-subtitle>
+                Pour x personnes
+            </v-card-subtitle>
+        </v-card-item>
+        <v-card-text>
+            <v-list>
+                <v-list-item v-for="n in 3" :key="n">Ingrédients</v-list-item>
+            </v-list>
+            <div>Étape 1 : allumer votre appareil...</div>
+        </v-card-text>
+        <v-divider class="mx-4 mb-1"></v-divider>
+        <v-card-actions>
+            <v-btn color="deep-purple-lighten-2" variant="text" @click="reserve">
+                Modifier la recette
+            </v-btn>
+        </v-card-actions>
+    </v-card>
+</template>
+
+<script>
+export default {
+    data: () => ({
+        loading: false,
+        selection: 1,
+    }),
+
+    methods: {
+        reserve() {
+            this.loading = true
+            setTimeout(() => (this.loading = false), 2000)
+        },
+    },
+}
+</script>

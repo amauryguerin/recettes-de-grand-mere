915edea1e0ea0c9ad4766bd313a8260422aa66c5
Amaury Guérin <amaugofast@MBP-de-Amaury.lan>
2023-10-08 17:04:12 +0200

    FRONT // branchement route put et ajout possibilité de modification

--- resources/js/components/RecipeListItem.vue
+++ resources/js/components/RecipeListItem.vue
@@ -1,49 +1,63 @@
 <template>
     <li class="recipe--item">
-        <v-card :loading="loading" class="mx-auto my-12" max-width="374">
+        <v-card class="mx-auto my-12" max-width="374">
             <v-img cover height="250" src="https://cdn.vuetifyjs.com/images/cards/cooking.png"></v-img>
             <v-card-item>
-                <v-card-title> {{ recipe.title }} </v-card-title>
+                <v-card-title v-if="!recipe.editing">{{
+                    recipe.title
+                }}</v-card-title>
+                <v-text-field v-model="recipe.title" v-else></v-text-field>
             </v-card-item>
             <v-card-text>
-                <p> {{ recipe.ingredient }} </p>
-                <p> {{ recipe.description }} </p>
+                <p v-if="!recipe.editing">{{ recipe.ingredient }}</p>
+                <v-text-field v-model="recipe.ingredient" v-else></v-text-field>
+                <p v-if="!recipe.editing">{{ recipe.description }}</p>
+                <v-text-field v-model="recipe.description" v-else></v-text-field>
             </v-card-text>
             <v-divider class="mx-4 mb-1"></v-divider>
             <v-card-actions>
-                <v-btn color="orange-lighten-1" variant="text" class="font-weight-bold">
-                    Modifier la recette
+                <v-btn color="orange-lighten-1" variant="text" class="font-weight-bold" @click="toggleEditing()">
+                    {{ recipe.editing ? "Enregistrer" : "Modifier la recette" }}
                 </v-btn>
                 <v-btn color="red" variant="text" icon="mdi-delete" @click="removeRecipe()"></v-btn>
             </v-card-actions>
         </v-card>
     </li>
 </template>
-
 <script>
 export default {
     props: ["recipe"],
     methods: {
+        toggleEditing() {
+            this.recipe.editing = !this.recipe.editing;
+            if (!this.recipe.editing) {
+                axios
+                    .put(`api/recipe/${this.recipe.id}`, { recipe: this.recipe })
+                    .then(res => { if (res.status == 200) { this.$emit("recipeChanged"); } })
+                    .catch(error => { console.log(error); })
+
+                this.$emit("recipeChanged");
+            }
+        },
         removeRecipe() {
             axios
                 .delete(`api/recipe/${this.recipe.id}`)
-                .then(res => {
+                .then((res) => {
                     if (res.status == 200) {
                         this.$emit("recipeChanged");
                     }
                 })
-                .catch(error => {
+                .catch((error) => {
                     console.log(error);
-                })
-        }
-    }
-}
+                });
+        },
+    },
+};
 </script>
-
 <style lang="scss">
-    .v-card-text {
-        display: flex;
-        flex-direction: column;
-        gap: 1rem;
-    }
+.v-card-text {
+    display: flex;
+    flex-direction: column;
+    gap: 1rem;
+}
 </style>
